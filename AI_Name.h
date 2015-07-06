87b436d0eef3896092931aa73f92d7349f085d12
Xujia Cao <xujiacao@169-231-116-205.wireless.ucsb.edu>
2015-07-05 21:12:34 -0700

    ai3 v2 still stupid

--- AI_Name.h
+++ AI_Name.h
@@ -18,7 +18,7 @@ const struct S_AI AIList[MAX_AI_NUM] = {
     {1,   ExampleAI       , "Example"},
     {2,   GTAIFunc        , "GaoTian AI"},
     {10,  ActualPlayer    , "Player"},
-    {7,   GaeBolg         , "Seraph"}
+    {7,   GaeBolg         , "Seraph"},
     {3,   Ai3             , "Ai3"},
 //    {5,   dummyProcedure  , "DummyIndex's Mashiro"},
 };
